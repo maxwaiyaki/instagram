483ecc96f40b2474ddf7a847ae52213e5fdfe97d
maxwaiyaki <maxwel.wairimu@gmail.com>
2018-12-14 18:12:50 +0300

    add Profile Model

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_picture = models.ImageField(upload_to = 'profile_pictures/', blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    bio = models.TextField(max_length = 100, blank=True)
+    followers = ManyToManyField(User,related_name='followers', blank=True)
